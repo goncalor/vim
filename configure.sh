f6b12c9f18a683c58c9af4b951ef49745b0565e4
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2018-12-29 12:38:58 +0000

    Add commentary.vim to the config script

--- configure.sh
+++ configure.sh
@@ -12,6 +12,10 @@ cp -v init.vim $confdir/init.vim
 mkdir -pv $confdir/autoload $confdir/bundle && \
 	curl -LSso $confdir/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+# download commentary.vim, by Tim Pope
+cd $confdir/bundle
+git clone git://github.com/tpope/vim-commentary.git
+
 # download sleuth.vim, by Tim Pope
 cd $confdir/bundle
 git clone git://github.com/tpope/vim-sleuth.git
